29948e53ca86c9bbc9b4114d43d6d1bbcde7e149
Gianfranco Manganiello <gmanganiello@teclib.com>
2018-08-06 18:20:38 -0400

    ci(deploy): select correct .gem file
    Signed-off-by: Gianfranco Manganiello <gmanganiello@teclib.com>

--- ci/scripts/publish_new_version.sh
+++ ci/scripts/publish_new_version.sh
@@ -3,4 +3,4 @@ npm run release
 export NEW_VERSION=$(jq -r ".version" package.json)
 ruby ./ci/scripts/change_plugin_version.rb $CURRENT_VERSION $NEW_VERSION
 gem build fastlane-plugin-xcov_report.gemspec
-gem push "fastlane-plugin-xcov_report-$NEW_VERSION.gem"
+gem push *.gem
